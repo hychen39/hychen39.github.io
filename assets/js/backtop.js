b5c443e5640866948b6e3b1d9402af39923ddef6
hychen39-T460S <hychen39@gmail.com>
2019-03-24 10:56:54 +0800

    Release JSF teaching unit 11

---
+++ assets/js/backtop.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    console.log(document.body.scrollTop);
+    console.log(document.documentElement.scrollTop);
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("backTopBtn").style.display = "block";
+  } else {
+    document.getElementById("backTopBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
